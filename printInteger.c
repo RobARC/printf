39b2591c5a91edb15ac8098a5a70aed74cd882fb
Reina <kdoro8540@gmail.com>
2020-10-27 21:18:09 -0500

    Update - Printf project

--- printInteger.c
+++ printInteger.c
@@ -1,25 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
 #include "holberton.h"
-
 /**
- * printInteger - Entry point
- * @ap: list of arguments to receive.
- * Return: ammount added to pointer.
- */
-
+* printInteger - Entry point
+* @ap: list of arguments to receive.
+* Return: ammount added to pointer.
+*/
 int printInteger(va_list ap)
 {
-int n = va_arg(ap, int);
-int last = n % 10;
-int exp = 1;
-int i = 1;
-int number, digt;
+	int n = va_arg(ap, int), last = n % 10, exp = 1;
 
-n = n / 10;
-number = n;
+	int i = 1, number, digt;
+
+	n = n / 10;
+
+	number = n;
 
 	if (last < 0)
 	{
@@ -29,25 +22,24 @@ number = n;
 		last = -last;
 		i++;
 	}
-	if (number > 0)
-	{
-		while (number / 10 != 0)
+		if (number > 0)
 		{
-			exp = exp * 10;
-			number = number / 10;
-		}
-		number = n;
-		while (exp > 0)
-		{
-			digt = number / exp;
-			printChar(digt + '0');
-			number = number - (digt * exp);
-			exp = exp / 10;
-			i++;
-		}
-	}
+			while (number / 10 != 0)
+			{
+				exp = exp * 10;
+				number = number / 10;
+			}
+			number = n;
 
-	printChar(last + '0');
-
-return (i);
+			while (exp > 0)
+			{
+				digt = number / exp;
+				printChar(digt + '0');
+				number = number - (digt * exp);
+				exp = exp / 10;
+				i++;
+			}
+		}
+		printChar(last + '0');
+		return (i);
 }
